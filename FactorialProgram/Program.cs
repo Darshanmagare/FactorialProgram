bf0cf07aabedfab10e47ddce0c43dc5b7a8328fd
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:09:37 +0530

    Add project files.

---
+++ FactorialProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FactorialProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialExample obj = new FactorialExample();
+            obj.factops();
+        }
+    }
+
+    public class FactorialExample
+    {
+        public void factops()
+        {
+            int i, fact = 1, number;
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+
+        }
+    }
+}
